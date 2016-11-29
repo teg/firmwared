c5f3a0d84501f6b2c9299bd3713ed2b8890e0dc8
Tom Gundersen <teg@jklm.no>
2016-11-29 01:38:40 +0100

    add firmware loading library
    The firmware loading library operate on a dirfd representing the
    device directory in /sys of the firmware device. It implements only
    two funcions, firmware_load() and firmware_cancel_load().
    
    The former takes an fd of the firmware file and copies this into the
    kernel. The latter indicate to the kernel that the firmware cannot
    be loaded.

---
+++ src/firmware.c
@@ -0,0 +1,103 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/sendfile.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "firmware.h"
+
+#define LOADING_START   (1)
+#define LOADING_CANCEL  (-1)
+#define LOADING_FINISH  (0)
+
+static int firmware_set_loading(int loadingfd, int state) {
+        int r;
+
+        r = dprintf(loadingfd, "%d\n", state);
+        if (r < 0)
+                return -errno;
+
+        return 0;
+}
+
+int firmware_load(int devicefd, int firmwarefd) {
+        int loadingfd = -1, datafd = -1;
+        struct stat statbuf;
+        int r;
+
+        loadingfd = openat(devicefd, "loading", O_CLOEXEC);
+        if (loadingfd < 0) {
+                r = -errno;
+                goto finish;
+        }
+
+        datafd = openat(devicefd, "data", O_CLOEXEC);
+        if (datafd < 0) {
+                r = -errno;
+                goto finish;
+        }
+
+        if (fstat(firmwarefd, &statbuf) < 0) {
+                r = -errno;
+                goto finish;
+        }
+
+        if (statbuf.st_size == 0) {
+                r = -EIO;
+                goto finish;
+        }
+
+        r = firmware_set_loading(loadingfd, LOADING_START);
+        if (r < 0)
+                goto finish;
+
+        while (statbuf.st_size) {
+                ssize_t size;
+                off_t offset = 0;
+
+                size = sendfile(datafd, firmwarefd, &offset, statbuf.st_size);
+                if (size < 0) {
+                        r = -errno;
+                        goto finish;
+                }
+
+                statbuf.st_size -= size;
+        }
+
+        firmware_set_loading(loadingfd, LOADING_FINISH);
+
+finish:
+        if (loadingfd >= 0)
+                close(loadingfd);
+        if (datafd >= 0)
+                close(datafd);
+        if (r < 0 && r != -ENOENT) {
+                firmware_set_loading(loadingfd, LOADING_CANCEL);
+                return r;
+        } else
+                return 0;
+}
+
+int firmware_cancel_load(int devicefd) {
+        int loadingfd = -1;
+        int r;
+
+        loadingfd = openat(devicefd, "loading", O_CLOEXEC);
+        if (loadingfd < 0) {
+                r = -errno;
+                goto finish;
+        }
+
+        r = firmware_set_loading(loadingfd, LOADING_CANCEL);
+        if (r < 0)
+                goto finish;
+
+finish:
+        if (loadingfd >= 0)
+                close(loadingfd);
+        if (r < 0 && r != -ENOENT)
+                return r;
+        else
+                return 0;
+}
---
+++ src/firmware.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int firmware_load(int devicefd, int firmwarefd);
+int firmware_cancel_load(int devicefd);
